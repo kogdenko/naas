d291804ea157a614206ec075faad7f44cc840f20
Konstantin Kogdenko <k.kogdenko@gmail.com>
2023-06-19 17:28:58 +0000

    pub to nats

--- vpp_sswan/updown.sh
+++ vpp_sswan/updown.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set charon.plugins.kernel-vpp.use_tunnel_mode_sa = no
-
 set -o nounset
 set -o errexit
 
@@ -15,10 +13,13 @@ case "${PLUTO_VERB}" in
 	echo "up-client"
 	echo "PLUTO_ME=${PLUTO_ME}"
 	echo "PLUTO_PEER=${PLUTO_PEER}"
+	echo "PLUTO_PEER_ID=${PLUTO_PEER_ID}"
 	echo "PLUTO_PEER_CLIENT=${PLUTO_PEER_CLIENT}"
 	echo "PLUTO_REQID=${PLUTO_REQID}"
 	echo "PLUTO_UNIQUEID=${PLUTO_UNIQUEID}"
 
+	nats --server 192.168.122.1 pub updown "add $IF ${PLUTO_PEER_CLIENT} ${PLUTO_PEER_ID} 1"
+	
 ##	vppctl "create ipip tunnel src ${PLUTO_ME} dst ${PLUTO_PEER} instance ${PLUTO_REQID} p2p"
 #	vppctl "ipsec itf create instance ${PLUTO_REQID}"
 #	vppctl "ipsec tunnel protect $IF sa-in 1 sa-out 2"
@@ -27,13 +28,15 @@ case "${PLUTO_VERB}" in
 	vppctl "ip route add ${PLUTO_PEER_CLIENT} via $IF"
         ;;
     down-client)
-
 	echo "down-client $IF"
 	echo "PLUTO_REQID=${PLUTO_REQID}"
 	echo "PLUTO_UNIQUEID=${PLUTO_UNIQUEID}"
 
 	vppctl "ip route del ${PLUTO_PEER_CLIENT} via $IF"
         vppctl "ipsec itf delete $IF"
+
+	nats --server 192.168.122.1 pub updown "del $IF ${PLUTO_PEER_CLIENT} ${PLUTO_PEER_ID} 1"
+
 #	sw_if_index=`vppctl "show interface $IF" | tail -1 | awk '{print $2}'`
 #	vppctl "delete ipip tunnel sw_if_index $sw_if_index"
         ;;
